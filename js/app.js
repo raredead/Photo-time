4f5d5fe16fcbc7fa26f4d0936be8b1761f97c891
raredead <smahelzed@gmail.com>
2023-08-29 15:42:49 +0700

    Исправлен адаптив, добавлен меню-бургер

---
+++ js/app.js
@@ -0,0 +1,6 @@
+const menuBtn = document.querySelector('.menu__btn');
+const menu = document.querySelector('.menu');
+
+menuBtn.addEventListener('click', function(){
+    menu.classList.toggle('active');
+});
