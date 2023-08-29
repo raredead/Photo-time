b99a5be616b3a40de5a3030a80205624a8fb4e91
raredead <smahelzed@gmail.com>
2023-08-29 18:36:02 +0700

    Доработал бургер меню

--- js/app.js
+++ js/app.js
@@ -1,6 +1,12 @@
 const menuBtn = document.querySelector('.menu__btn');
 const menu = document.querySelector('.menu');
+const body = document.querySelector('body');
 
-menuBtn.addEventListener('click', function(){
-    menu.classList.toggle('active');
+menuBtn.addEventListener('click', function(e){
+    e.stopPropagation();
+    menu.classList.add('active');
+});
+
+body.addEventListener('click', function(){
+    menu.classList.remove('active');
 });
